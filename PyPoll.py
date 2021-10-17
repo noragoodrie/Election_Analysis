9fb0d48642abe8b95808cd58f52cfef1b16d81bc
Nora Goodrie <noragoodrie@Noras-MacBook-Air.local>
2021-10-17 15:38:21 -0700

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,44 @@
+import csv
+import os
+
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # To do: read and analyze the data here
+    
+#Read the file object witht the reader function
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+     # Write three counties to the file.
+     txt_file.write("Arapahoe\nDenver\nJefferson")
+    
+
+
+
+
+
+
